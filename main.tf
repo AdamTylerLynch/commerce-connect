a629986ea3a6b9418e35bc3c8d5517d1cfc7a1a2
Tyler Lynch <=>
2022-05-20 15:39:57 -0400

    added simply inbound flow.

--- main.tf
+++ main.tf
@@ -6,3 +6,41 @@ resource "aws_connect_instance" "commerce" {
   auto_resolve_best_voices_enabled = false
   contact_flow_logs_enabled        = true
 }
+
+resource "aws_connect_contact_flow" "commerce-inbound-default" {
+  instance_id  = aws_connect_instance.commerce.id
+  name         = "Commerce Inbound Flow"
+  description  = "Default inbound call flow"
+  type         = "CONTACT_FLOW"
+
+   content     = <<JSON
+       {
+       "Version": "2019-10-30",
+       "StartAction": "ACTION001",
+       "Actions": [ 
+         {
+           "Identifier": "ACTION001",
+           "Type": "MessageParticipant",
+           "Transitions": {
+             "NextAction": "ACTION900",
+             "Errors": [],
+             "Conditions": []
+           },
+           "Parameters": {
+             "Text": "Welcome to Commerce Company."
+           }
+         },
+         {
+           "Identifier": "ACTION900",
+           "Type": "DisconnectParticipant",
+           "Transitions": {},
+           "Parameters": {}
+         }
+       ]
+     }
+     JSON
+
+  tags = {
+    "Name"   = "Commerce Inbound Flow"
+  }
+}
