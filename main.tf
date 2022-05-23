126b2ae0b6ea1c4da144157ebb21d1264495e3de
Tyler Lynch <=>
2022-05-23 10:43:00 -0400

    added lex bot association

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+data "aws_region" "current" {}
 resource "aws_connect_instance" "commerce" {
   identity_management_type         = "CONNECT_MANAGED"
   instance_alias                   = "commerce-connect"
@@ -42,5 +43,51 @@ resource "aws_connect_contact_flow" "commerce-inbound-default" {
 
   tags = {
     "Name"   = "Commerce Inbound Flow"
+  }
+}
+
+resource "aws_lex_intent" "commerce-inbound-order" {
+  create_version = true
+  name           = "connect_commerce_lex_intent"
+  fulfillment_activity {
+    type = "ReturnIntent"
+  }
+  sample_utterances = [
+    "I would like to order flowers",
+    "I would like to order candies",
+  ]
+}
+
+
+resource "aws_lex_bot" "commerce-inbound-order" {
+  abort_statement {
+    message {
+      content      = "Sorry, I am not able to assist at this time"
+      content_type = "PlainText"
+    }
+  }
+  clarification_prompt {
+    max_attempts = 2
+    message {
+      content      = "I didn't understand you, what would you like to do?"
+      content_type = "PlainText"
+    }
+  }
+  intent {
+    intent_name    = aws_lex_intent.commerce-inbound-order.name
+    intent_version = "1"
+  }
+
+  child_directed   = false
+  name             = "connect_commerce_lex_bot"
+  process_behavior = "BUILD"
+}
+
+resource "aws_connect_bot_association" "commerce-inbound-order" {
+  instance_id = aws_connect_instance.commerce.id
+  lex_bot {
+    lex_region = data.aws_region.current.name
+    name       = aws_lex_bot.commerce-inbound-order.name
+
   }
 }
