c58af6aa2e9e3917c1ece8636198cadfbc37df6c
Tyler Lynch <=>
2022-05-24 14:09:04 -0400

    updated the definition

--- main.tf
+++ main.tf
@@ -3,6 +3,5 @@ resource "aws_connect_instance" "commerce" {
   instance_alias                   = "commerce-connect"
   inbound_calls_enabled            = true
   outbound_calls_enabled           = true
-  auto_resolve_best_voices_enabled = false
   contact_flow_logs_enabled        = true
 }
